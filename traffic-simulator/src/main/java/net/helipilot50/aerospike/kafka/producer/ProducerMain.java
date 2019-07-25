049949a9efc73593313c62d130e5fc8064a08e1c
Peter Milne <Peter.Milne@adform.com>
2019-07-25 15:05:01 +0200

    updated producer to count url visits

--- traffic-simulator/src/main/java/net/helipilot50/aerospike/kafka/producer/ProducerMain.java
+++ traffic-simulator/src/main/java/net/helipilot50/aerospike/kafka/producer/ProducerMain.java
@@ -9,8 +9,11 @@ import com.aerospike.client.AerospikeClient;
 import com.aerospike.client.Bin;
 import com.aerospike.client.Key;
 import com.aerospike.client.Record;
+import com.aerospike.client.Value;
 import com.aerospike.client.Operation;
 import com.aerospike.client.AerospikeException.Connection;
+import com.aerospike.client.cdt.MapOperation;
+import com.aerospike.client.cdt.MapPolicy;
 
 import net.helipilot50.aerospike.kafka.Constants;
 
@@ -20,29 +23,56 @@ import net.helipilot50.aerospike.kafka.Constants;
  */
 public class ProducerMain extends TimerTask {
 
-    private static final List<User> users = new ArrayList<User>() {
+    private static final List<String> users = new ArrayList<String>() {
         private static final long serialVersionUID = 408064932585810852L;
 
         {
-            add(new User("bob-123", "Bob", 0L));
-            add(new User("mary-456", "Mary", 0L));
-            add(new User("sue-789", "Sue", 0L));
-            add(new User("james-189", "James", 0L));
-            add(new User("sarah-289", "Sarah", 0L));
-            add(new User("mark-281", "Mark", 0L));
+            add("bob-123");
+            add("mary-456");
+            add("sue-789");
+            add("james-189");
+            add("sarah-289");
+            add("mark-281");
         }
     };
 
-    private static User randomUser() {
+    private static final List<String> sites = new ArrayList<String>() {
+        private static final long serialVersionUID = 1L;
+        {
+            add("http://some.place.com/app");
+            add("http://some.place.com/app/catalog");
+            add("http://some.place.com/app/catalog/bike");
+            add("http://some.place.com/app/catalog/scooter");
+            add("http://some.place.com/app/catalog/doll");
+            add("http://some.place.com/app/catalog/teddybare");
+            add("http://some.place.com/app//catalog/banana");
+            add("http://some.place.com/app/catalog/pair");
+            add("http://some.place.com/app/catalog/lemon");
+            add("http://some.place.com/app/catalog/pineapple");
+            add("http://some.place.com/app/catalog/lego");
+            add("http://some.place.com/app/catalog/paint");
+            add("http://some.place.com/app/catalog/cloth");
+            add("http://some.place.com/app/catalog/blocks");
+            add("http://some.place.com/app/catalog/bread");
+            add("http://some.place.com/app/catalog/glue");
+        }
+    };
+
+    private static String randomUser() {
         int index = (int) (Math.random() * users.size());
         return users.get(index);
+    }
+
+    private static String randomSite() {
+        int index = (int) (Math.random() * users.size());
+        return sites.get(index);
     }
 
     public static void main(String[] args) {
         System.out.println("Aerospike Kafka Producer");
         ProducerMain producer = new ProducerMain();
         Timer timer = new Timer();
-        timer.schedule(producer, 0, 5000);
+        timer.schedule(producer, 0, 1000);
     }
 
     public AerospikeClient asClient;
@@ -53,19 +83,19 @@ public class ProducerMain extends TimerTask {
         while (attemptConnection) {
             attempts += 1;
             try {
-                System.out.println("Connect to aerospike, attempt: " + attempts );
-                this.asClient = new AerospikeClient("localhost", 3000);
-                break;
+                System.out.println("Connect to aerospike, attempt: " + attempts);
+                this.asClient = new AerospikeClient("aerospike", 3000);
+                attemptConnection = false;
             } catch (Connection conn) {
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
-                if (attempts > 5) {
+                if (attempts > 2) {
                     attemptConnection = false;
-                    System.out.println("Cannot connect to aerospike, attempted: " + attempts );
+                    System.out.println("Cannot connect to aerospike, attempted: " + attempts);
                     throw conn;
                 }
             }
@@ -74,24 +104,23 @@ public class ProducerMain extends TimerTask {
 
     @Override
     public void run() {
-        User user = randomUser();
-        System.out.println("Writing to Aerospike: " + user.id);
-        incrementVisits(user.id, user.name);
+        String user = randomUser();
+        String siteUrl = randomSite();
+        System.out.println(String.format("Visit: %s to %s", user, siteUrl));
+        addVisit(user, siteUrl);
     }
 
-    public User incrementVisits(String userId, String name) {
+    public void addVisit(String userId, String siteUrl) {
 
-        // Create a key
         Key recordKey = new Key(Constants.NAMESPACE, Constants.RECORD_SET, userId);
 
-        // Increment operations
-        Bin nameBin = new Bin(Constants.NAME_BIN, name);
-        Bin visitBin = new Bin(Constants.VISIT_BIN, 1);
-
-        // https://www.aerospike.com/docs/client/java/usage/kvs/multiops.html#operation-specification
-        Record record = asClient.operate(null, recordKey, Operation.add(visitBin), Operation.put(nameBin),
-                Operation.get(Constants.NAME_BIN), Operation.get(Constants.VISIT_BIN));
-        return new User(userId, record.getString(Constants.NAME_BIN), record.getLong(Constants.VISIT_BIN));
+        asClient.operate(null, recordKey, MapOperation.increment(
+            MapPolicy.Default, 
+            Constants.VISIT_BIN, 
+            Value.get(siteUrl),
+            Value.get(1)
+            ));
+        return;
     }
 
     /**
